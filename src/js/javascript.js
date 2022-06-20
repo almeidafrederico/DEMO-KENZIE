a51adb85e5e7c03293085e649bc7670f2baa00df
Frederico S Almeida <almeidaa.frederico@gmail.com>
2022-06-20 10:40:47 -0300

    s4-04 - Atividades - Funções: Exercitando o uso de parâmetro

--- src/js/javascript.js
+++ src/js/javascript.js
@@ -1,13 +1,24 @@
-function soma(a, b) {
-    return a + b;
+function somarDoisValores(Valor1, Valor2) {
+    return Valor1 + Valor2;
 }
 
-let result = soma(2, 4);
+console.log(somarDoisValores(15,876));
 
-console.log(result);
-
-function digaOla(){
-    alert("Olá");
+function tipoValor(valor) {
+    let tipo = typeof(valor);
+    text = `O valor (${valor}) é do tipo (${tipo})`
+    return text
 }
 
-digaOla();
+console.log(tipoValor(true))
+
+function somarNotas(nota1, nota2, nota3, nota4) {
+    if(typeof(nota1) != "number" || typeof(nota2) != "number" || typeof(nota3) != "number"|| typeof(nota4) != "number"){
+        return "O campo só pode receber números"
+    }else{
+        return nota1+nota2+nota3+nota4;
+    }
+    
+}
+
+console.log(somarNotas(8,"6",9,3))
