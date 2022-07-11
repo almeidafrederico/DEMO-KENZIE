7bf1a9f1ea154e62cda6b8a3fbe887752bd09ccd
Frederico S Almeida <almeidaa.frederico@gmail.com>
2022-07-11 10:51:07 -0300

    apredendo JS eventos

--- src/js/javascript.js
+++ src/js/javascript.js
@@ -4,3 +4,20 @@ let livroObj = {
   paginas: 208,
   codigo: 15648977,
 }
+
+
+const btnProdutos1 = document.querySelectorAll("button");
+
+console.log(btnProdutos1);
+
+btnProdutos1[0].addEventListener("click", click1);
+btnProdutos1[1].addEventListener("click", click2);
+
+
+function click1(event){
+  console.log("click1")
+}
+
+function click2(event){
+  console.log("click2")
+}
